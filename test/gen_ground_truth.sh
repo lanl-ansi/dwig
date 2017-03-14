0e7fc5f87d75d5754af159cc166060c03702be80
Carleton Coffrin <carleton@coffr.in>
2017-03-13 21:50:07 -0600

    Incorporation of the bqpjson package (#27)
    * cleaning out bqpjson code
    * cleaning up documentation
    * updating requirements and bqpjson version number
    * fixing link to bqpjson spec in readme

--- test/gen_ground_truth.sh
+++ test/gen_ground_truth.sh
@@ -1,6 +1,4 @@
 rm data/*.json
-rm data/*.qh
-rm data/*.qubo
 
 ../dwig.py -cd  1 -tl -rs 0 ran > data/ran1_i_1.json
 ../dwig.py -cd  1 -tl -rs 0 ran -f > data/ran1_i_2.json
@@ -15,9 +13,3 @@ rm data/*.qubo
 ../dwig.py -cd 6 -tl -rs 0 wscn > data/wscn_i_1.json
 ../dwig.py -cd 9 -tl -rs 0 wscn > data/wscn_i_2.json
 ../dwig.py -cd 12 -tl -rs 0 wscn > data/wscn_i_3.json
-
-for file in $(find data -name *.json); do
-    cat $file | ../bqp2qh.py > ${file//.json/.qh}
-    cat $file | ../spin2bool.py | ../bqp2qubo.py > ${file//.json/.qubo}
-    cat $file | ../bqp2mzn.py > ${file//.json/.mzn}
-done
