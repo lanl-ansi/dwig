93743f7b7c9f3ead6c1b78429e8d0324c4cf50c0
Carleton <cjc@lanl.gov>
2017-03-27 16:14:43 -0600

    updating tests suite to be multi-line to improve future diffs

--- test/fcl_test.py
+++ test/fcl_test.py
@@ -10,15 +10,15 @@ class TestFLGeneration:
     def setup_class(self):
         self.parser = dwig.build_cli_parser()
 
-    #../dwig.py -cd 6 -tl -rs 0 fl -cc -a 0.1 > data/fcl_i_1.json
+    #../dwig.py -pp -cd 6 -tl -rs 0 fl -cc -a 0.1 > data/fcl_i_1.json
     def test_fl_i_1(self, capfd):
-        json_comp(self.parser, capfd, 'fcl_i_1.json', ['-cd', '6', '-tl', '-rs', '0', 'fl', '-cc', '-a', '0.1'])
+        json_comp(self.parser, capfd, 'fcl_i_1.json', ['-pp', '-cd', '6', '-tl', '-rs', '0', 'fl', '-cc', '-a', '0.1'])
 
-    #../dwig.py -cd 6 -tl -rs 0 fl -cc -s 4 > data/fcl_i_2.json
+    #../dwig.py -pp -cd 6 -tl -rs 0 fl -cc -s 4 > data/fcl_i_2.json
     def test_fl_i_2(self, capfd):
-        json_comp(self.parser, capfd, 'fcl_i_2.json', ['-cd', '6', '-tl', '-rs', '0', 'fl', '-cc', '-s', '4'])
+        json_comp(self.parser, capfd, 'fcl_i_2.json', ['-pp', '-cd', '6', '-tl', '-rs', '0', 'fl', '-cc', '-s', '4'])
 
-    #../dwig.py -cd 12 -tl -rs 1 fl -cc > data/fcl_i_3.json
+    #../dwig.py -pp -cd 12 -tl -rs 1 fl -cc > data/fcl_i_3.json
     def test_fl_i_3(self, capfd):
-        json_comp(self.parser, capfd, 'fcl_i_3.json', ['-cd', '12', '-tl', '-rs', '1', 'fl', '-cc'])
+        json_comp(self.parser, capfd, 'fcl_i_3.json', ['-pp', '-cd', '12', '-tl', '-rs', '1', 'fl', '-cc'])
 
--- test/ran_test.py
+++ test/ran_test.py
@@ -10,20 +10,20 @@ class TestRanGeneration:
     def setup_class(self):
         self.parser = dwig.build_cli_parser()
 
-    #../dwig.py -cd 1 -tl -rs 0 -form ising ran > data/ran1_i_1.json
+    #../dwig.py -pp -cd 1 -tl -rs 0 -form ising ran > data/ran1_i_1.json
     def test_ran1_i_1(self, capfd):
-        json_comp(self.parser, capfd, 'ran1_i_1.json', ['-cd', '1', '-tl', '-rs', '0', 'ran'])
+        json_comp(self.parser, capfd, 'ran1_i_1.json', ['-pp', '-cd', '1', '-tl', '-rs', '0', 'ran'])
 
-    #../dwig.py -cd 1 -tl -rs 0 -form ising ran -f > data/ran1_i_2.json
+    #../dwig.py -pp -cd 1 -tl -rs 0 -form ising ran -f > data/ran1_i_2.json
     def test_ran1_i_2(self, capfd):
-        json_comp(self.parser, capfd, 'ran1_i_2.json', ['-cd', '1', '-tl', '-rs', '0', 'ran', '-f'])
+        json_comp(self.parser, capfd, 'ran1_i_2.json', ['-pp', '-cd', '1', '-tl', '-rs', '0', 'ran', '-f'])
 
-    #../dwig.py -cd 1 -tl -rs 0 -form ising ran -s 4 > data/ran4_i_0.json
+    #../dwig.py -pp -cd 1 -tl -rs 0 -form ising ran -s 4 > data/ran4_i_0.json
     def test_ran4_i_0(self, capfd):
-        json_comp(self.parser, capfd, 'ran4_i_0.json', ['-cd', '1', '-tl', '-rs', '0', 'ran', '-s', '4'])
+        json_comp(self.parser, capfd, 'ran4_i_0.json', ['-pp', '-cd', '1', '-tl', '-rs', '0', 'ran', '-s', '4'])
 
-    #../dwig.py -cd 12 -tl -rs 0 ran > data/ran1_i_3.json
+    #../dwig.py -pp -cd 12 -tl -rs 0 ran > data/ran1_i_3.json
     def test_ran1_i_3(self, capfd):
-        json_comp(self.parser, capfd, 'ran1_i_3.json', ['-cd', '12', '-tl', '-rs', '0', 'ran'])
+        json_comp(self.parser, capfd, 'ran1_i_3.json', ['-pp', '-cd', '12', '-tl', '-rs', '0', 'ran'])
 
 
