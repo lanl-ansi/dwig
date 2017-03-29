4472abb39d65c036b01495a434212d3d1f04accd
Carleton Coffrin <carleton@coffr.in>
2017-03-29 09:59:44 -0600

    Updating FL algorithms to use random ground state (#29)
    * adding random planted solution option, closes #18
    * adding new tests

--- test/fcl_test.py
+++ test/fcl_test.py
@@ -4,21 +4,43 @@ sys.path.append('.')
 import dwig
 
 from common_test import json_comp
+from common_test import run_dwig_cli
 
 
-class TestFLGeneration:
+class TestFCLGeneration:
     def setup_class(self):
         self.parser = dwig.build_cli_parser()
 
-    #../dwig.py -pp -cd 6 -tl -rs 0 fl -cc -a 0.1 > data/fcl_i_1.json
-    def test_fl_i_1(self, capfd):
-        json_comp(self.parser, capfd, 'fcl_i_1.json', ['-pp', '-cd', '6', '-tl', '-rs', '0', 'fl', '-cc', '-a', '0.1'])
+    #../dwig.py -pp -cd 6 -tl -rs 0 fl -sgs -cc -a 0.1 > data/fcl_i_1.json
+    def test_fcl_i_1(self, capfd):
+        json_comp(self.parser, capfd, 'fcl_i_1.json', ['-pp', '-cd', '6', '-tl', '-rs', '0', 'fl', '-sgs', '-cc', '-a', '0.1'])
 
-    #../dwig.py -pp -cd 6 -tl -rs 0 fl -cc -s 4 > data/fcl_i_2.json
-    def test_fl_i_2(self, capfd):
-        json_comp(self.parser, capfd, 'fcl_i_2.json', ['-pp', '-cd', '6', '-tl', '-rs', '0', 'fl', '-cc', '-s', '4'])
+    #../dwig.py -pp -cd 6 -tl -rs 0 fl -sgs -cc -s 4 > data/fcl_i_2.json
+    def test_fcl_i_2(self, capfd):
+        json_comp(self.parser, capfd, 'fcl_i_2.json', ['-pp', '-cd', '6', '-tl', '-rs', '0', 'fl', '-sgs', '-cc', '-s', '4'])
 
-    #../dwig.py -pp -cd 12 -tl -rs 1 fl -cc > data/fcl_i_3.json
-    def test_fl_i_3(self, capfd):
-        json_comp(self.parser, capfd, 'fcl_i_3.json', ['-pp', '-cd', '12', '-tl', '-rs', '1', 'fl', '-cc'])
+    #../dwig.py -pp -cd 12 -tl -rs 1 fl -sgs -cc > data/fcl_i_3.json
+    def test_fcl_i_3(self, capfd):
+        json_comp(self.parser, capfd, 'fcl_i_3.json', ['-pp', '-cd', '12', '-tl', '-rs', '1', 'fl', '-sgs', '-cc'])
 
+    #../dwig.py -pp -cd 6 -tl -rs 0 fl -cc -a 0.1 > data/fcl_i_4.json
+    def test_fcl_i_4(self, capfd):
+        json_comp(self.parser, capfd, 'fcl_i_4.json', ['-pp', '-cd', '6', '-tl', '-rs', '0', 'fl', '-cc', '-a', '0.1'])
+
+
+class TestFCLGroundState:
+    def setup_class(self):
+        self.parser = dwig.build_cli_parser()
+
+    #../dwig.py -pp -cd 6 -tl -rs 0 fl -sgs -cc -a 0.1 > data/fcl_i_1.json
+    def test_eq(self, capfd):
+        cli_args = ['-pp', '-cd', '6', '-tl', '-rs', '0', 'fl', '-cc', '-a', '0.1']
+
+        json_base = run_dwig_cli(self.parser, cli_args)
+
+        cli_args.append('-sgs')
+        json_sgs = run_dwig_cli(self.parser, cli_args)
+
+        base_solution_eval = json_base['solutions'][0]['evaluation']
+        sgs_solution_eval = json_sgs['solutions'][0]['evaluation']
+        assert(abs(base_solution_eval - sgs_solution_eval) <= 1e-8)
--- test/fl_test.py
+++ test/fl_test.py
@@ -4,24 +4,47 @@ sys.path.append('.')
 import dwig
 
 from common_test import json_comp
+from common_test import run_dwig_cli
 
 
 class TestFLGeneration:
     def setup_class(self):
         self.parser = dwig.build_cli_parser()
 
-    #../dwig.py -pp -cd 2 -tl -rs 0 fl > data/fl_i_1.json
+    #../dwig.py -pp -cd 2 -tl -rs 0 fl -sgs > data/fl_i_1.json
     def test_fl_i_1(self, capfd):
-        json_comp(self.parser, capfd, 'fl_i_1.json', ['-pp', '-cd', '2', '-tl', '-rs', '0', 'fl'])
+        json_comp(self.parser, capfd, 'fl_i_1.json', ['-pp', '-cd', '2', '-tl', '-rs', '0', 'fl', '-sgs'])
 
-    #../dwig.py -pp -cd 2 -tl -rs 0 fl -s 4 -a 0.3 > data/fl_i_2.json
+    #../dwig.py -pp -cd 2 -tl -rs 0 fl -sgs -s 4 -a 0.3 > data/fl_i_2.json
     def test_fl_i_2(self, capfd):
-        json_comp(self.parser, capfd, 'fl_i_2.json', ['-pp', '-cd', '2', '-tl', '-rs', '0', 'fl', '-s', '4', '-a', '0.3'])
+        json_comp(self.parser, capfd, 'fl_i_2.json', ['-pp', '-cd', '2', '-tl', '-rs', '0', 'fl', '-sgs', '-s', '4', '-a', '0.3'])
 
-    #../dwig.py -pp -cd 2 -tl -rs 0 fl -mc -mll 0 > data/fl_i_3.json
+    #../dwig.py -pp -cd 2 -tl -rs 0 fl -sgs -mc -mll 0 > data/fl_i_3.json
     def test_fl_i_3(self, capfd):
-        json_comp(self.parser, capfd, 'fl_i_3.json', ['-pp', '-cd', '2', '-tl', '-rs', '0', 'fl', '-mc', '-mll', '0'])
+        json_comp(self.parser, capfd, 'fl_i_3.json', ['-pp', '-cd', '2', '-tl', '-rs', '0', 'fl', '-sgs', '-mc', '-mll', '0'])
 
-    #../dwig.py -pp -cd 12 -tl -rs 0 fl > data/fl_i_4.json
+    #../dwig.py -pp -cd 12 -tl -rs 0 fl -sgs > data/fl_i_4.json
     def test_fl_i_4(self, capfd):
-        json_comp(self.parser, capfd, 'fl_i_4.json', ['-pp', '-cd', '12', '-tl', '-rs', '0', 'fl'])
+        json_comp(self.parser, capfd, 'fl_i_4.json', ['-pp', '-cd', '12', '-tl', '-rs', '0', 'fl', '-sgs'])
+
+    #../dwig.py -pp -cd  2 -tl -rs 0 fl > data/fl_i_5.json
+    def test_fl_i_5(self, capfd):
+        json_comp(self.parser, capfd, 'fl_i_5.json', ['-pp', '-cd', '2', '-tl', '-rs', '0', 'fl'])
+
+
+class TestFLGroundState:
+    def setup_class(self):
+        self.parser = dwig.build_cli_parser()
+
+    #../dwig.py -pp -cd 2 -tl -rs 0 fl -sgs > data/fl_i_1.json
+    def test_eq(self, capfd):
+        cli_args = ['-cd', '2', '-tl', '-rs', '0', 'fl']
+
+        json_base = run_dwig_cli(self.parser, cli_args)
+
+        cli_args.append('-sgs')
+        json_sgs = run_dwig_cli(self.parser, cli_args)
+
+        base_solution_eval = json_base['solutions'][0]['evaluation']
+        sgs_solution_eval = json_sgs['solutions'][0]['evaluation']
+        assert(abs(base_solution_eval - sgs_solution_eval) <= 1e-8)
