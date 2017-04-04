a825c9cdbf21fe534cb3e95b25a66e6adca29644
Carleton <cjc@lanl.gov>
2017-04-04 13:20:27 -0600

    updating tests to reflect merger of rfm into ran

--- test/ran_test.py
+++ test/ran_test.py
@@ -12,18 +12,18 @@ class TestRanGeneration:
 
     #../dwig.py -pp -cd 1 -tl -rs 0 -form ising ran > data/ran1_i_1.json
     def test_ran1_i_1(self, capfd):
-        json_comp(self.parser, capfd, 'ran1_i_1.json', ['-pp', '-cd', '1', '-tl', '-rs', '0', 'ran'])
+        json_comp(self.parser, capfd, 'ran1_i_1.json', ['-pp','-cd', '1', '-tl', '-rs', '0', '-os', 'ran'])
 
     #../dwig.py -pp -cd 1 -tl -rs 0 -form ising ran -f > data/ran1_i_2.json
     def test_ran1_i_2(self, capfd):
-        json_comp(self.parser, capfd, 'ran1_i_2.json', ['-pp', '-cd', '1', '-tl', '-rs', '0', 'ran', '-f'])
+        json_comp(self.parser, capfd, 'ran1_i_2.json', ['-pp', '-cd', '1', '-tl', '-rs', '0', '-os', 'ran', '-f'])
 
     #../dwig.py -pp -cd 1 -tl -rs 0 -form ising ran -s 4 > data/ran4_i_0.json
     def test_ran4_i_0(self, capfd):
-        json_comp(self.parser, capfd, 'ran4_i_0.json', ['-pp', '-cd', '1', '-tl', '-rs', '0', 'ran', '-s', '4'])
+        json_comp(self.parser, capfd, 'ran4_i_0.json', ['-pp', '-cd', '1', '-tl', '-rs', '0', '-os', 'ran', '-s', '4'])
 
     #../dwig.py -pp -cd 12 -tl -rs 0 ran > data/ran1_i_3.json
     def test_ran1_i_3(self, capfd):
-        json_comp(self.parser, capfd, 'ran1_i_3.json', ['-pp', '-cd', '12', '-tl', '-rs', '0', 'ran'])
+        json_comp(self.parser, capfd, 'ran1_i_3.json', ['-pp', '-cd', '12', '-tl', '-rs', '0', '-os', 'ran'])
 
 
--- test/rfm_test.py
+++ test/rfm_test.py
@@ -12,12 +12,12 @@ class TestRFMGeneration:
 
     #../dwig.py -pp -cd 1 -tl -rs 0 rfm > data/rfm1_i_1.json
     def test_rfm_i_1(self, capfd):
-        json_comp(self.parser, capfd, 'rfm1_i_1.json', ['-pp', '-cd', '1', '-tl', '-rs', '0', 'rfm'])
+        json_comp(self.parser, capfd, 'rfm1_i_1.json', ['-pp', '-cd', '1', '-tl', '-rs', '0', 'ran', '-a', '0.0'])
 
     #../dwig.py -pp -cd 1 -tl -rs 0 rfm -f > data/rfm1_i_2.json
     def test_rfm1_i_2(self, capfd):
-        json_comp(self.parser, capfd, 'rfm1_i_2.json', ['-pp', '-cd', '1', '-tl', '-rs', '0', 'rfm', '-f'])
+        json_comp(self.parser, capfd, 'rfm1_i_2.json', ['-pp', '-cd', '1', '-tl', '-rs', '0', 'ran', '-f', '-a', '0.0'])
 
     #../dwig.py -pp -cd 1 -tl -rs 0 rfm -f -s 4 > data/rfm4_i_1.json
     def test_rfm3_i_1(self, capfd):
-        json_comp(self.parser, capfd, 'rfm4_i_1.json', ['-pp', '-cd', '1', '-tl', '-rs', '0', 'rfm', '-f', '-s', '4'])
+        json_comp(self.parser, capfd, 'rfm4_i_1.json', ['-pp', '-cd', '1', '-tl', '-rs', '0', 'ran', '-f', '-s', '4', '-a', '0.0'])
